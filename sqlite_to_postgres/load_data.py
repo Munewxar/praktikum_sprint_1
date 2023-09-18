d6bb7ddcb610160580d9e19fb4a79efd05f51693
Steven Altamirano <steven.altamirano.games@gmail.com>
2023-09-18 16:02:19 +0300

    connections closing added

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -4,6 +4,8 @@ import psycopg2
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 
+from contextlib import closing
+
 import logger
 from db_worker import TABLE_NAME_TO_TYPE, PostgresSaver, SQLiteExtractor
 
@@ -47,8 +49,8 @@ def run_data_transfer() -> None:
         'port': 5432
     }
 
-    with sqlite3.connect('db.sqlite') as sqlite_conn, \
-            psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+    with closing(sqlite3.connect('db.sqlite')) as sqlite_conn, \
+            closing(psycopg2.connect(**dsl, cursor_factory=DictCursor)) as pg_conn:
 
         load_from_sqlite(sqlite_conn, pg_conn)
 
